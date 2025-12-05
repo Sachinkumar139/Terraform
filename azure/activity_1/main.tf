4d11eee299cc9094d0ca74def2abb6b2f19babfa
Your Name <sachinchittodiya139@gmail.com>
2025-12-05 15:10:46 +0530

    added map

--- azure/activity_1/main.tf
+++ azure/activity_1/main.tf
@@ -1,26 +1,26 @@
 resource "azurerm_resource_group" "base" {
-    name = var.resource_group_info.name
-    location = var.resource_group_info.location
-  
+  name     = var.resource_group_info.name
+  location = var.resource_group_info.location
+
 }
 # virtual network
 
 resource "azurerm_virtual_network" "main" {
-    name = var.network_info.name
-    address_space = [var.network_info.address_space]
-    location = azurerm_resource_group.base.location
-    resource_group_name = azurerm_resource_group.base.name
-    tags = {
-        Environment = "Dev"
-    }
+  name                = var.network_info.name
+  address_space       = [var.network_info.address_space]
+  location            = azurerm_resource_group.base.location
+  resource_group_name = azurerm_resource_group.base.name
+  tags = {
+    Environment = "Dev"
+  }
 
-    depends_on = [ azurerm_resource_group.base ]  
+  depends_on = [azurerm_resource_group.base]
 }
 resource "azurerm_subnet" "subnets" {
-    count = length(var.network_info.subnets)
-    name = var.network_info.subnets[count.index].name
-    address_prefixes = [var.network_info.subnets[count.index].address_space]
-    resource_group_name = azurerm_resource_group.base.name
-    virtual_network_name = azurerm_virtual_network.main.name
-    depends_on = [ azurerm_virtual_network.main ] 
+  count                = length(var.network_info.subnets)
+  name                 = var.network_info.subnets[count.index].name
+  address_prefixes     = [var.network_info.subnets[count.index].address_space]
+  resource_group_name  = azurerm_resource_group.base.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  depends_on           = [azurerm_virtual_network.main]
 }
--- azure/activity_1/variables.tf
+++ azure/activity_1/variables.tf
@@ -1,87 +1,87 @@
 variable "resource_group_info" {
-    type = object({
-      name = string
-      location = string 
-    })
-    description = "resource group information"
-    default = {
-      location = "eastus"
-      name = "ntier"
-    }
-}    
+  type = object({
+    name     = string
+    location = string
+  })
+  description = "resource group information"
+  default = {
+    location = "eastus"
+    name     = "ntier"
+  }
+}
 variable "network_info" {
-    type = object({
-        name = string
-        address_space = string
-        subnets = list(object({
-            name = string
-            address_space = string
-        }))
-    })
-    default = {
-        name = "ntier"
-        address_space = "192.168.0.0/16"
-        subnets = [ {
-          name = "web"
-          address_space = "192.168.0.0/24"
-        },{
-            name = "app"
-            address_space = "192.168.1.0/24"
-        } , {
-            name = "db"
-            address_space = "192.168.2.0/24"
-        }]
-    }
+  type = object({
+    name          = string
+    address_space = string
+    subnets = list(object({
+      name          = string
+      address_space = string
+    }))
+  })
+  default = {
+    name          = "ntier"
+    address_space = "192.168.0.0/16"
+    subnets = [{
+      name          = "web"
+      address_space = "192.168.0.0/24"
+      }, {
+      name          = "app"
+      address_space = "192.168.1.0/24"
+      }, {
+      name          = "db"
+      address_space = "192.168.2.0/24"
+    }]
+  }
 }
 
 variable "web_security_group" {
-    type = object({
-      name = string
-      rules = list(object({
-        name = string
-        priority = number
-        direction = string
-        protocol = string
-        source_port_range = string
-        destination_port_range = string
-        source_address_prefix = string
-        destination_address_prefix = string
-        access = string
-      }))
-    })
-    default = {
-      name = "web"
-      rules = [ {
-        name = "webssh"
-        priority = 300
-        direction = "Inbound"
-        protocol = "Tcp"
-        source_port_range = "*"
-        destination_port_range = "22"
-        source_address_prefix = "*"
+  type = object({
+    name = string
+    rules = map(object({
+      priority                   = number
+      direction                  = string
+      protocol                   = string
+      source_port_range          = string
+      destination_port_range     = string
+      source_address_prefix      = string
+      destination_address_prefix = string
+      access                     = string
+    }))
+  })
+  default = {
+    name = "web"
+    rules = {
+      "webssh" = {
+        priority                   = 300
+        direction                  = "Inbound"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
         destination_address_prefix = "*"
-        access = "Allow"
-      },{
-        name = "webhttp"
-        priority = 310
-        direction = "Inbound"
-        protocol = "Tcp"
-        source_port_range = "*"
-        destination_port_range = "80"
-        source_address_prefix = "*"
+        access                     = "Allow"
+      },
+      "webhttp" = {
+        priority                   = 310
+        direction                  = "Inbound"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
         destination_address_prefix = "*"
-        access = "Allow"
-      },{
-        name = "webhttps"
-        priority = 320
-        direction = "Inbound"
-        protocol = "Tcp"
-        source_port_range = "*"
-        destination_port_range = "443"
-        source_address_prefix = "*"
-        destination_address_prefix = "*" 
-        access = "Allow"     
-        } ]
+        access                     = "Allow"
+      },
+      "webhttps" = {
+        priority                   = 320
+        direction                  = "Inbound"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "443"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+        access                     = "Allow"
+      }
     }
-  
+  }
+
 }
